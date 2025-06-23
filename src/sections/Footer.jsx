f45e33c9d7fb6f07819426a49c228d1087ac539d
Kirill <kushovk2003@mail.ru>
2025-06-23 20:58:01 +0500

    fix

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -15,8 +15,8 @@ const Footer = () => {
           <div className="flex flex-col gap-4">
             {footerItems.map((footerItem, i) => (
               <div key={i}>
-                <h1 className="text-[16px] text-white font-semibold">
-                  {footerItem.title}
+                <h1 className="text-[16px] text-white cursor-pointer hover:text-[#C0B7E8] transition-all duration-300 font-semibold">
+                  {footerItem.title} 
                 </h1>
               </div>
             ))}
@@ -26,7 +26,7 @@ const Footer = () => {
           <div className="flex flex-col gap-4">
             {footerConditions.map((footerContidion, i) => (
               <div key={i} className="">
-                <h1 className="text-[16px] text-white font-semibold">
+                <h1 className="text-[16px] text-white cursor-pointer hover:text-[#C0B7E8] transition-all duration-300 font-semibold">
                   {footerContidion.title}
                 </h1>
               </div>
@@ -35,7 +35,7 @@ const Footer = () => {
           <div className="self-stretch hidden lg:flex w-[7px] bg-gradient-to-t from-transparent via-[#C0B7E8] to-transparent" />
           <div className="self-stretch flex lg:hidden h-[7px] bg-gradient-to-r from-transparent via-[#C0B7E8] to-transparent" />
           <div className="flex flex-col items-center lg:items-start gap-6">
-            <h1 className="text-[16px] text-white font-semibold">
+            <h1 className="text-[18px] text-white font-semibold">
               SOCIALIZE WITH HYDRA
             </h1>
             <div className="flex gap-3">
@@ -43,7 +43,7 @@ const Footer = () => {
                 const IconPath = footerIcon.icon;
                 return (
                   <div key={i}>
-                    <IconPath className="size-8 text-[#C0B7E8]" />
+                    <IconPath className="size-8 text-[#C0B7E8] cursor-pointer hover:text-[#ffffff] transition-all duration-300" />
                   </div>
                 );
               })}
@@ -55,7 +55,7 @@ const Footer = () => {
           <div className="h-[2px] w-full bg-gradient-to-r from-transparent via-[#C0B7E8] to-transparent" />
         </div>
         <div className="flex-center">
-          <p className="text-sm text-white font-semibold">
+          <p className="text-sm text-white/40 font-semibold">
             {new Date().getFullYear()} &#169; HYDRA LANDING PAGE - BY ZINE. E.
             FALOUTI - ALL RIGHTS RESERVED{" "}
           </p>
